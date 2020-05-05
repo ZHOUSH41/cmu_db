ea8e9f755d6e1ad660d54e8e2a30fdda2206bede
zhou <624061566@qq.com>
2020-05-05 20:24:58 +0800

    code base

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhou/Documents/cmu_15445_2018
+# Build directory: /home/zhou/Documents/cmu_15445_2018/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("src")
+subdirs("test")
---
+++ cmake-build-debug/src/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zhou/Documents/cmu_15445_2018/src
+# Build directory: /home/zhou/Documents/cmu_15445_2018/cmake-build-debug/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
